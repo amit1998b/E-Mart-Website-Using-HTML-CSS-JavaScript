0a4c29deef675f7dd01c07b227fc06e36340689d
amit1998b <108368019+amit1998b@users.noreply.github.com>
2023-03-02 17:08:40 +0530

    Add files via upload

---
+++ Front End Project/script.js
@@ -0,0 +1,12 @@
+// Hamberger menu
+
+const bar = document.getElementById("bar");
+const nav = document.getElementById("Navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+
